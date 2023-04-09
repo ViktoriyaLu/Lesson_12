387e38f83e9895f436cb42592f60c23563e84394
ViktoriyaLu <114606124+ViktoriyaLu@users.noreply.github.com>
2023-04-09 19:36:03 +0300

    Start

---
+++ assets/index.js
@@ -0,0 +1,25 @@
+const displayName = document.querySelector('.display-name');
+const displayAvatar = document.querySelector('.display-avatar');
+const displayComment = document.querySelector('.display-comment');
+const userDateName = document.querySelector('.userDate__Name');
+const userDateAvatar = document.querySelector('.userDate__avatar');
+const userDateComment = document.querySelector('.userDate__comment');
+const send = document.querySelector('.button'); //кнопка
+// console.log(displayName);
+// console.log(displayAvatar);
+// console.log(displayComment);
+// console.log(userDateAvatar);
+// console.log(userDateComment);
+// console.log(send);
+
+const userName = displayName.textContent;
+const avatar = displayAvatar.textContent;
+const comment = displayComment.textContent;
+// console.log(userName);
+// console.log(avatar);
+// console.log(comment);
+send.addEventListener('click', () =>{
+userDateName.textContent = userName
+});
+
+console.log (userDateName.textContent);
